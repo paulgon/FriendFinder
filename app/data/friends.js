2d6a33119bf6f5fc0b27cf3c1d36180ccb91f59e
paulgonteacher <50177334+paulgon@users.noreply.github.com>
2019-09-29 18:04:46 -0500

    added friend pics

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,32 +2,32 @@ var friends = [
     {
         name: "James",
         photo:
-            "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/7/78/Charlie_Kelly.jpg",
+            "https://static.livebooks.com/b19f3fd86e454826bcae2e1410ada3ca/i/e1100d0473914dd2bc4556e5ee8ef835/1/4SoifmQp7LJ6yDtMuFY2x/man.jpg",
         scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 5]
     },
     {
         name: "Peter",
         photo:
-            "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/5/5f/Mac_-_Season_8.jpg",
+            "https://images.squarespace-cdn.com/content/v1/5c869a3765a70789cd2f45cb/1561643779801-LKQIZY6PBA9PZTGUDCZP/ke17ZwdGBToddI8pDm48kGMVL505f8vOX5b5IpNZTOZ7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmjJyaSXAwc_BJvYKv4pjNutsWQerjLa-zX8YC-jXkkl1Qt0VpPILZd7GZ7pMD1QbD/Headshot-man.jpg",
         scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
     },
     {
         name: "Samantha",
         photo:
-            "https://hips.hearstapps.com/cosmouk.cdnds.net/16/24/1466084818-sweet-dee-sunny-philadelphia.jpg",
+            "https://images.squarespace-cdn.com/content/v1/551706d7e4b07674243fc0d7/1555349158373-WENPZ5B921FZFVNIFPI1/ke17ZwdGBToddI8pDm48kLkXF2pIyv_F2eUT9F60jBl7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0iyqMbMesKd95J-X4EagrgU9L3Sa3U8cogeb0tjXbfawd0urKshkc5MgdBeJmALQKw/DSC_2949.jpg?format=1500w",
         scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 5]
     },
 
     {
         name: "Brooke",
         photo:
-            "https://i.pinimg.com/originals/96/3c/a7/963ca7ed3b3c08d90af0d962fadd49de.jpg",
+            "https://www.dialteg.com/wp-content/uploads/2018/10/Pretty-Woman-Staring-First-Impression.jpg",
         scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 5]
     },
     {
         name: "Louis",
         photo:
-            "https://i.pinimg.com/originals/b7/d0/41/b7d041d02dfd02ee421079720db79f2e.jpg",
+            "https://www.orau.org/img/stem/staffing-man.jpg",
         scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 5]
     }
 ];
