31ec5e9e6932c85bea60f79dc599ce6b75482b74
paulgonteacher <50177334+paulgon@users.noreply.github.com>
2019-09-29 16:58:38 -0500

    added public folder

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+var friends = [
+    {
+        name: "Charlie",
+        photo:
+            "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/7/78/Charlie_Kelly.jpg",
+        scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 5]
+    },
+    {
+        name: "Mac",
+        photo:
+            "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/5/5f/Mac_-_Season_8.jpg",
+        scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+    },
+    {
+        name: "Dee",
+        photo:
+            "https://hips.hearstapps.com/cosmouk.cdnds.net/16/24/1466084818-sweet-dee-sunny-philadelphia.jpg",
+        scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 5]
+    },
+
+    {
+        name: "Dennis",
+        photo:
+            "https://i.pinimg.com/originals/96/3c/a7/963ca7ed3b3c08d90af0d962fadd49de.jpg",
+        scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 5]
+    },
+    {
+        name: "Frank",
+        photo:
+            "https://i.pinimg.com/originals/b7/d0/41/b7d041d02dfd02ee421079720db79f2e.jpg",
+        scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 5]
+    }
+];
+module.exports = friends;
